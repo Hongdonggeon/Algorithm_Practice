2136027629b5d4f94292ad0f17bfcbef52f0a291
Hongdonggeon <ghd700@naver.com>
2021-07-10 13:01:59 +0900

    큰 수의 법칙

---
+++ ThisisCodingTest/greedy_practice02.py
@@ -0,0 +1,19 @@
+n, m, k = map(int, input().split()) // 한번에 입력받기
+array = list(map(int, input().split()))
+
+array.sort()
+biggest = array[n-1]
+secondBig = array[n-2]
+
+total = 0
+while True:
+    for i in range(k):
+        if m==0:
+            break
+        total += biggest
+        m-=1
+    if m ==0:
+        break
+    total+=secondBig
+    m-=1
+print(total)
