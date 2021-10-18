a629010ebdaad498cb9154252609fddd1997b1b3
Hongdonggeon <ghd700@naver.com>
2021-10-18 12:00:07 +0900

    상하좌우

--- ThisisCodingTest/Implementation/Example01.java
+++ ThisisCodingTest/Implementation/Example01.java
@@ -4,41 +4,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Example01 {
-    static boolean checkLandU(int x, int y, String input) {
-        if (x == 1 && "U".equals(input))
-            return false;
-        if (y == 1 && "L".equals(input))
-            return false;
-        return true;
-    }
-
-    static boolean checkRandD(int x, int y, int max, String input) {
-        if (x == max && "R".equals(input))
-            return false;
-        if (y == max && "D".equals(input))
-            return false;
-        return true;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int maxLength = Integer.parseInt(st.nextToken());
-        int initX = 1;
-        int initY = 1;
-        st = new StringTokenizer(br.readLine(), " ");
+        int initX = 1, initY = 1;
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        char[] directions = {'L', 'R', 'U', 'D'};
 
+        st = new StringTokenizer(br.readLine());
         while (st.hasMoreTokens()) {
-            String input = st.nextToken();
-            if ("R".equals(input) && checkRandD(initX, initY, maxLength, input))
-                initY++;
-            else if ("D".equals(input) && checkRandD(initX, initY, maxLength, input))
-                initX++;
-            else if ("U".equals(input) && checkLandU(initX, initY, input))
-                initX--;
-            else if ("L".equals(input) && checkLandU(initX, initY, input))
-                initY--;
+            char direction = st.nextToken().charAt(0);
+            int nx = 0, ny = 0;
+            for (int i = 0; i < 4; i++) {
+                if (direction == directions[i]) {
+                    nx = initX + dx[i];
+                    ny = initY + dy[i];
+                }
+            }
+            if (nx < 1 || ny < 1 || nx > maxLength || ny > maxLength)
+                continue;
+            initX = nx;
+            initY = ny;
         }
         System.out.println(initX + " " + initY);
     }
